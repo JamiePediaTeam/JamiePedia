a7443201eb202d16239fba0c693d4d0cc4e5f09d
Xtra3678 <tjcomer7@gmail.com>
2026-02-21 23:48:05 -0500

    machine love update to new template

--- assets/static/song.js
+++ assets/static/song.js
@@ -13,6 +13,27 @@ function switchTab(tabName) {
   event.target.classList.add('active');
 }
 
+function switchAlbumArt(filename) {
+  // Update the image source
+  document.getElementById('album-art-image').src = '../../public/images/cover-art/' + filename;
+  // Remove active state from all album art tabs
+  document.querySelectorAll('.album-tab').forEach(el => {
+    el.classList.remove('active');
+  });
+  // Add active state to clicked tab
+  event.target.classList.add('active');
+  
+  // Update cover artist based on selected album art
+  const coverArtists = {
+    'ml.png': 'ReverieQue',
+    'cc.png': 'ReverieQue',
+    'ccde.png': 'ReverieQue',
+    'ddoll.jpg': 'Crispy6usiness',
+    'bdkt26.png': 'Kurumitsu'
+  };
+  document.getElementById('cover-artist-display').textContent = coverArtists[filename] || 'Jamie Paige';
+}
+
 function previousSong() {
   alert('Previous song functionality to be implemented');
 }
